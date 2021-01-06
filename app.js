e63515fe85d33485338332d9aac8d275e0fe887a
Emtiaz2000 <62500633+Emtiaz2000@users.noreply.github.com>
2021-01-06 11:12:48 +0600

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const jsText = document.querySelector('#input-js-text');
 const button =  document.querySelector('#button');
 const ifram =  document.querySelector('#final-output');
 
+//try to make custom tab button
+htmlText.addEventListener('keydown',(e)=>{
+    if(e.key=='Tab'){
+        e.preventDefault()
+        console.log('tab')
+    }
+})
 button.addEventListener('click',function getOutput(){
     console.log(htmlText.value)
     ifram.contentDocument.body.innerHTML = htmlText.value;
